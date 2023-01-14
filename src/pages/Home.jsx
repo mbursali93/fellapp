0781008d9425f67c578ea490eab90265e49bc0be
Mustafa Bursali <m.bursali16@gmail.com>
2023-01-15 01:06:13 +0300

    deploy test

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 import ParticleBackground from "../components/ParticleBackground"
 
 
-import "./styles/homeStyles.css"
+import homeStyles from "./styles/homeStyles.css"
 
 export default function Home() {
 
